5f59041f5b5691ea3746be10d0692c1cfce0c47e
neitzen <nolaneitzen@gmail.com>
2020-02-27 17:51:01 -0600

    Edited STATIC_ROOT

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_kd)r1$drb76mcux1+dit8on=1huvucw-=ck^7pgk77uic6o63'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
