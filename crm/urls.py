428ca9af7007e9e4648b07b8f47f6a76f4018299
neitzen <nolaneitzen@gmail.com>
2020-04-17 15:12:20 -0500

    Added RestAPI

--- crm/urls.py
+++ crm/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.conf.urls import url
 from . import views
 from django.urls import path, re_path
+from rest_framework.urlpatterns import format_suffix_patterns
+
 
 app_name = 'crm'
 urlpatterns = [
@@ -18,4 +21,7 @@ urlpatterns = [
     path('product/<int:pk>/delete/', views.product_delete, name='product_delete'),
     path('product/create/', views.product_new, name='product_new'),
     path('customer/<int:pk>/summary/', views.summary, name='summary'),
+    url(r'^customers_json/', views.CustomerList.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
